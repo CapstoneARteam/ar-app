ae46a91c37a7e291eb147dd810cedc741250f0ea
unknown <choliaky@gmail.com>
2020-07-23 01:10:55 -0700

    Updates

---
+++ precache-manifest.dee16316193b533f88d38e573fc4ccf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aa8eca3fae2d2ff90c305bbea22741",
+    "url": "/ar-app/index.html"
+  },
+  {
+    "revision": "5891c4d74d766bc45a6a",
+    "url": "/ar-app/static/css/2.4c51a07b.chunk.css"
+  },
+  {
+    "revision": "94c6d93818b311fd28cc",
+    "url": "/ar-app/static/css/main.f95672be.chunk.css"
+  },
+  {
+    "revision": "5891c4d74d766bc45a6a",
+    "url": "/ar-app/static/js/2.58a80dfc.chunk.js"
+  },
+  {
+    "revision": "1f28fc3eee0a645225bdaffd4c69bdd7",
+    "url": "/ar-app/static/js/2.58a80dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c6d93818b311fd28cc",
+    "url": "/ar-app/static/js/main.051086ff.chunk.js"
+  },
+  {
+    "revision": "5a2ee13be5ecb630e2f3",
+    "url": "/ar-app/static/js/runtime-main.19ee092f.js"
+  }
+]);
