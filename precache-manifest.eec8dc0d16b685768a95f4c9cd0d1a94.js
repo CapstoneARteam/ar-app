2df22775860adff3c6b835e012abfd3cf5b04881
Geoffrey <geoolson@pdx.edu>
2020-08-19 17:58:51 -0700

    Updates

---
+++ precache-manifest.eec8dc0d16b685768a95f4c9cd0d1a94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620de7bb5dbd7f9110140cd8574b8566",
+    "url": "/ar-app/index.html"
+  },
+  {
+    "revision": "f9ede15af4dae3861cfc",
+    "url": "/ar-app/static/css/2.4c51a07b.chunk.css"
+  },
+  {
+    "revision": "e569cb6f6982df89cad4",
+    "url": "/ar-app/static/css/main.4d5e8915.chunk.css"
+  },
+  {
+    "revision": "f9ede15af4dae3861cfc",
+    "url": "/ar-app/static/js/2.73172fc6.chunk.js"
+  },
+  {
+    "revision": "cfe785ec557bce1db1242a7e490d273d",
+    "url": "/ar-app/static/js/2.73172fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e569cb6f6982df89cad4",
+    "url": "/ar-app/static/js/main.1c5712c6.chunk.js"
+  },
+  {
+    "revision": "f9fe46b8d08030297e16",
+    "url": "/ar-app/static/js/runtime-main.2abd9314.js"
+  }
+]);
